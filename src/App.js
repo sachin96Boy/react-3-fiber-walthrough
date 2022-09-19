ebb289d069e7c56dae105888ac1db132d067c891
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-09-19 14:56:51 +0530

    hello world- react-3-fiber

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber';
+import DummyElem from './components/DummyElem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Canvas>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <DummyElem position={[-1.2, 0, 0]} />
+    <DummyElem position={[1.2, 0, 0]} />
+   </Canvas>
   );
 }
 
